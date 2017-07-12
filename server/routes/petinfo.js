db0ea4272e68e906391a0bb4e0cb63672140d272
Benjaminhaesemeyer <benjaminhaesemeyer@gmail.com>
2017-07-12 16:52:25 -0500

    updated the petinfo.js

--- server/routes/petinfo.js
+++ server/routes/petinfo.js
@@ -10,6 +10,52 @@ var config = {
   idleTimeOutMillis: 30000,
 };
 
+var pool = new pg.Pool(config);
 
+router.get('/', function(req, res){
+
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+      var queryText = 'SELECT * FROM "pets";';
+      db.query(queryText, function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          res.send({pets: result.rows});
+        }
+      }); // end query
+    } // end if
+  }) // end pool
+});
+router.post('/', function(req, res) {
+  var newPet = req.body;
+  console.log(newPet);
+
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+      var queryText = 'INSERT INTO "pets" ("pet_name", "breed", "color", "owner_id")' +
+                      ' VALUES ($1, $2, $3, $4);';
+
+      db.query(queryText, [newPet.pet_name, newPet.breed, newPet.color, newPet.owner_id], function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      }); // end query
+    } // end if
+  }) // end pool
 
 module.exports = router;
