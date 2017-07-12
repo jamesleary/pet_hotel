14adf6874b9fbeffa38359114dbaffffd61ea464
Benjaminhaesemeyer <benjaminhaesemeyer@gmail.com>
2017-07-12 15:08:29 -0500

    catch all is now working

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 var port = process.env.PORT || 5000;
-
-app.use(express.static('server/public'));
-
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/*', function(req, res){
+  var file = req.params[0] || '/views/index.html';
+res.sendFile(path.join(__dirname, "/public/", file));
+});
 
 app.listen(port, function(){
   console.log('starting application');
